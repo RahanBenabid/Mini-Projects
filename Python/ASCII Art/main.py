dfbee39ebd6edb7d90f17ae47ec5bf1e76f9c8fc
RahanBenabid <rahannadime@gmail.com>
2024-12-05 22:32:51 +0100

    ASCII Conveter final

--- Python/ASCII Art/main.py
+++ Python/ASCII Art/main.py
@@ -1,44 +1,58 @@
 #!/usr/bin/env python3
 
 from PIL import Image
+import math
+import os
 
-imageURL = './image_resized.jpg'
+imageURL = './../../../../../Downloads/images/--raycast/pexels-emmali-6589610.jpg'
+# from thinnest to boldest
+ASCII = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+RATIO = (len(ASCII) - 1) /255
+
+print(RATIO)
 
 
-
-def average(rgb_matrix):
-	brightness_matrix = []
-	for rgb in rgb_matrix:
-		brightness = (rgb[0] + rgb[1] + rgb[2]) / 3
-		brightness_matrix.append(brightness)
-	return brightness_matrix
+def average(rgb):
+    return (rgb[0] + rgb[1] + rgb[2]) / 3
 
 def lightness():
-	pass
-
+    pass
+    
 def luminosity():
-	pass
+    pass
 
-with Image.open(imageURL) as img:
-	# step 1: read the image and get the dimentions
-	print(f'Image size: {img.width} x {img.height}')
-	print(img.mode)
-	rgb_img = img.convert('RGB')
-	rgb_matrix = []
+def resize_image(img, terminal_width):
+    aspect_ratio = img.height / img.width
+    new_width = terminal_width
+    new_height = int(new_width * aspect_ratio * 0.4)
+    return img.resize((new_width, new_height))
 
-	# step 2: load the images pixels RGB data into a 2d array
-	rgb_matrix.append(rgb_img.getpixel((0, 1)))
-	print(rgb_matrix)
-	
-	print('Iterating through pixel content...')
-	for x in range(img.width):
-		for y in range(img.height):
-			rgb_matrix.append(rgb_img.getpixel((x, y)))
-	print('RGB matrix created!')
-	
-	# convert the RGB matrix into a brightness matrix
-	print('creating the brightness matrix...')
-	brightness_matrix = average(rgb_matrix)
-	print('Brightness matrix created!')
-	
-	
+try:
+    with Image.open(imageURL) as img:
+        # step 1: read the image and get the dimentins
+        print(f'Image size: {img.width} x {img.height}')
+        print(f'Image mode: {img.mode}')
+        
+        # step 2: resize based on the terminal size
+        terminal_width, _ = os.get_terminal_size()
+        resized_img = resize_image(img, terminal_width)
+        print(f'resized image size: {resized_img.width} x {resized_img.height}')
+        
+        rgb_img = resized_img.convert('RGB')
+        
+        ascii_drawing = []
+        for y in range(resized_img.height):
+            for x in range(resized_img.width):
+                rgb = rgb_img.getpixel((x, y))
+                brightness = average(rgb)
+                ascii_index = min(math.floor(brightness * RATIO), len(ASCII) - 1)
+                ascii_drawing.append(ASCII[ascii_index])
+            ascii_drawing.append('\n')
+        ascii_drawing = ''.join(ascii_drawing)
+        
+        print(ascii_drawing)
+        
+except FileNotFoundError:
+    print("Error: image not found")
+except IOError:
+    print("Error: invalid image file")
